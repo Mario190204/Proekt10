e2adc64e072ff6212b826d1a78a171c5e0a7c530
Mario1902 <dimitrievskimario@gmail.com>
2024-01-23 11:32:07 +0100

    Proekt10

---
+++ src/Minimum/Minimum.java
@@ -0,0 +1,18 @@
+package Minimum;
+
+public class Minimum {
+
+	public static int findMin(int n1, int n2, int n3) {
+		int min;
+		if(n1<n2 && n1<n3)
+			min = n1;
+		else if(n2 < n3)
+			min = n2;
+		else
+			min = n3;
+		return min;
+	}
+	public static void main(String[]args) {
+		System.out.println("Minimum od trite broja e:" + findMin(10, 7, 2));
+		}
+}
